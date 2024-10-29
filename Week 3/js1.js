402bf2b59e94711238bc3f569d5edc2e6fe50a3a
kunal bhalerao <kunalbhalerao789@gmail.com>
2024-10-29 15:48:36 +0530

    refreshing javascript and DOM

--- Week 3/js1.js
+++
@@ -1,6 +0,0 @@
-function sum(a, b){
-	return a + b;
-}
-
-var ans = sum(2, 4)
-console.log(ans)
---
+++ Week 4/index.js
@@ -0,0 +1,6 @@
+// const chalk = require("chalk");
+import chalk from "chalk";
+
+console.log(chalk.blue("Hello World!"));
+console.log(chalk.red.bold.underline("This is an error"));
+console.log(chalk.green.underline("This is an succes script!"));
