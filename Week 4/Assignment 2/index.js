5f9b7d69556e935ea7c23e0c51c51c99cd58ad06
kunal bhalerao <kunalbhalerao789@gmail.com>
2024-10-31 20:05:02 +0530

    solving assignment no 1 and assignment no 2 is pending

---
+++ Week 4/Assignment 2/index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const { Command } = require("commander");
+const program = new Command();
+
+program.name("todo").description("file base todo app");
+version("0.8.0");
+
+program
+  .command("add")
+  .description("add a todo title and display it.")
+  .argument("<string>", "save the todo title in todos file")
+  .action((str) => {
+    
+  })
