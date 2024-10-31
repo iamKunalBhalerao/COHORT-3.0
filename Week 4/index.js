405fe5724e04bcf269e14274a5ba9e45e01b821b
kunal bhalerao <kunalbhalerao789@gmail.com>
2024-10-31 07:54:35 +0530

    Learning git and use of chalk package and debugging things

--- Week 4/index.js
+++ Week 4/index.js
@@ -1,1 +1,7 @@
+// const chalk = require("chalk");
+import chalk from "chalk";
 console.log("This is Node js from scratch");
+
+console.log(chalk.blue("hello World!"));
+console.log(chalk.red.bold("This is as error"));
+console.log(chalk.green.underline.bold("This is as success message"));
