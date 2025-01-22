eaacad79831688a18ef5bcc4899b69571750a2a6
kunal bhalerao <kunalbhalerao789@gmail.com>
2025-01-22 23:51:59 +0530

    deleting node module folder

--- Week-6 offline/some practice and revicing back/index.js
+++ Week-6 offline/some practice and revicing back/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
-const jwt_password = "Thisispasswordprotected";
+const jwt_password = "thisisjsonwebtokenpassword";
 app.use(express.json());
 
 const users = [];
@@ -23,7 +23,6 @@ app.post("/signup", (req, res) => {
     msg: "You have signed up",
   });
 });
-
 app.post("/signin", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -32,12 +31,13 @@ app.post("/signin", (req, res) => {
 
   if (user) {
     const token = jwt.sign({ username }, jwt_password);
+
     res.status(200).json({
       token,
     });
   } else {
     res.status(403).json({
-      msg: "Invalid Crendentials!!!",
+      msg: "INvalid Crenditials",
     });
   }
 });
@@ -48,11 +48,10 @@ app.use((req, res, next) => {
 
   if (userDetails.username) {
     req.username = userDetails.username;
-    console.log("You are Logged in");
     next();
   } else {
     res.status(403).json({
-      msg: "You are not Logged IN",
+      msg: "Unauthorized",
     });
   }
 });
@@ -61,14 +60,9 @@ app.get("/me", (req, res) => {
   const user = users.find((user) => user.username === req.username);
 
   res.status(200).json({
+    msg: "welcome " + user.username,
     username: user.username,
     password: user.password,
-  });
-});
-
-app.get("/just", (req, res) => {
-  res.json({
-    msg: "This is just try for fun",
   });
 });
 
