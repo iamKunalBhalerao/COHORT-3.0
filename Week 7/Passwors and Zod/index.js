887cfdf6aa59eabe7d83aefb47e4f1720d3e393b
kunal bhalerao <kunalbhalerao789@gmail.com>
2025-02-01 02:57:30 +0530

    adding password hashing using bcrypt and Input Validation using zod libreary

--- Week 7/Passwors and Zod/index.js
+++ Week 7/Passwors and Zod/index.js
@@ -1,3 +1,5 @@
+const { z } = require("zod");
+const bcrypt = require("bcrypt");
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
@@ -15,14 +17,59 @@ app.post("/signup", async (req, res) => {
   const password = req.body.password;
   const name = req.body.name;
 
-  await UserModel.create({
-    email: email,
-    password: password,
-    name: name,
+  // Input Validation
+  const requireBody = z.object({
+    email: z.string().min(5).max(100).email(),
+    name: z.string().min(3).max(100),
+    password: z
+      .string()
+      .min(3)
+      .max(100)
+      .refine((value) => /[A-Z]/.test(value), {
+        message: "Password must contain at least one uppercase letter",
+      })
+      .refine((value) => /[a-z]/.test(value), {
+        message: "Password must contain at least one lowercase letter",
+      })
+      .refine((value) => /\d/.test(value), {
+        message: "Password must contain at least one digit",
+      })
+      .refine((value) => /[!@#$%^&*(),.?":{}|<>]/.test(value), {
+        message: "Password must contain at least one special character",
+      }),
   });
 
+  // const parsedData = requireBody.parse(req.body);
+  const parsedDatawithSuccess = requireBody.safeParse(req.body);
+
+  if (!parsedDatawithSuccess.success) {
+    res.status(403).json({
+      message: "Invalid email or password",
+      error: parsedDatawithSuccess.error.errors,
+    });
+    return;
+  }
+
+  // Hashing the password using bcrypt and error handling (server crash) usign try and chatch
+  let errorthrown = false;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 5);
+
+    await UserModel.create({
+      email: email,
+      password: hashedPassword,
+      name: name,
+    });
+  } catch (e) {
+    res.status(403).json({
+      message: "User Alerdy Exists 🤬",
+    });
+    errorthrown = true;
+    return;
+  }
+
   res.status(200).json({
-    message: "You are Signed Up",
+    message: "You are Signed Up 👏",
   });
 });
 
@@ -31,11 +78,18 @@ app.post("/signin", async (req, res) => {
   const password = req.body.password;
 
   const user = await UserModel.findOne({
-    email,
-    password,
+    email: email,
   });
 
-  if (user) {
+  if (!user) {
+    res.status(403).json({
+      message: "Your credentials are Invalid",
+    });
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+
+  if (passwordCompare) {
     const token = jwt.sign({ id: user._id.toString() }, jwt_password);
     res.status(200).json({
       token,
