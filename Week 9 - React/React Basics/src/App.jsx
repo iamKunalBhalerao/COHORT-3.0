5f6e0c75709af4a53c6863042f260785864fd722
iamKunalBhalerao <kunalbhalerao789@gmail.com>
2025-02-11 14:32:54 +0530

    Created a very ugaly looking todo like app

--- Week 9 - React/React Basics/src/App.jsx
+++ Week 9 - React/React Basics/src/App.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import { useState } from "react";
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      title: "Go and Code",
+      description: "Code a Todo app",
+      done: false,
+    },
+  ]);
+
+  function addTodo() {
+    let newArray = [];
+    for (let i = 0; i < todos.length; i++) {
+      newArray.push(todos[i]);
+    }
+
+    newArray.push({
+      title: "Go to Barber shop",
+      description: "Cut hair properly",
+      done: false,
+    });
+    setTodos(newArray);
+  }
+
   return (
     <>
-      <h1>Hello World</h1>
+      <div>
+        <button onClick={addTodo}>Add Todo</button>
+        <p>{JSON.stringify(todos)}</p>
+      </div>
     </>
   );
 };
---
+++ Week 9 - React/React Basics/src/components/Todo.jsx
@@ -0,0 +1,9 @@
+const Todo = () => {
+  return (
+    <>
+      <h1>This is Todo app</h1>
+    </>
+  );
+};
+
+export default Todo;
