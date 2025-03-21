61994f06afcb99f3fd81e64370191a79e940c7c3
iamKunalBhalerao <kunalbhalerao789@gmail.com>
2025-03-21 23:44:21 +0530

    Learned about useEffect it used to resolve some errors about mounting and rendering

---
+++ Week 9 - React/useEffect/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Notification from "./components/Notification";
+import Greeting from "./components/Greeting";
+
+const App = () => {
+  return (
+    <div>
+      <Notification />
+      {/* <Greeting name={"Kunal"} age={19} /> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ Week 9 - React/useEffect/src/components/Notification.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+const Notification = () => {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount((prevValue) => prevValue + 1);
+  };
+
+  useEffect(() => {
+    setInterval(increase, 1000);
+  }, []);
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          margin: "100px",
+        }}
+      >
+        <img
+          style={{
+            position: "relative",
+          }}
+          src="https://cdn-icons-png.flaticon.com/512/3119/3119338.png"
+          alt="notification"
+          width={80}
+        />
+        <span
+          style={{
+            position: "absolute",
+            padding: 8,
+            borderRadius: "100px",
+            backgroundColor: "crimson",
+            color: "white",
+          }}
+        >
+          {count}
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default Notification;
