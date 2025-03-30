c314c1a7b3c68356768d4fa5a1858b46568a9421
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-03-30 21:21:29 +0530

    Learned about Context API in React js

--- Week 10/Prp Drilling/src/App.jsx
+++ Week 10/Prp Drilling/src/App.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, { createContext, useContext, useState } from "react";
+
+const BulbContext = createContext();
+
+const BulbContextProvider = ({ children }) => {
+  const [bulbState, setBulbState] = useState(true);
+  return (
+    <>
+      <BulbContext.Provider value={{ bulbState, setBulbState }}>
+        {children}
+      </BulbContext.Provider>
+    </>
+  );
+};
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <BulbContextProvider>
+        <Light />
+      </BulbContextProvider>
+    </>
+  );
+};
 
-export default App
+const Light = () => {
+  return (
+    <>
+      <div style={{ padding: "2rem" }}>
+        <Bulb />
+        <ToggleBulb />
+      </div>
+    </>
+  );
+};
+
+const Bulb = () => {
+  const { bulbState } = useContext(BulbContext);
+
+  return (
+    <>
+      <h1>Bulb is {bulbState ? "On" : "Off"}</h1>
+    </>
+  );
+};
+
+const ToggleBulb = () => {
+  const { setBulbState } = useContext(BulbContext);
+
+  function ToggleBulb() {
+    setBulbState((prev) => !prev);
+  }
+
+  return (
+    <>
+      <button onClick={ToggleBulb}>Toggle</button>
+    </>
+  );
+};
+
+export default App;
