f4f7cbe5db8511b6d8d3e155974e682f7b6bf675
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-05-31 21:19:24 +0530

    Stsrting Tailwind CSS from COHORT 2.0

---
+++ MixedUp/Tailwind CSS - COHORT 2.0/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="flex justify-between">
+        <div className="bg-red-500">Hi Their from first div</div>
+        <div className="bg-yellow-500">Hi Their from second div</div>
+        <div className="bg-blue-500">Hi Their from third div</div>
+      </div>
+    </>
+  );
+};
+
+export default App;
