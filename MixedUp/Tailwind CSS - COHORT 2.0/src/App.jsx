25f96bdd551c111f14952c7c312ebf206cf73072
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-05-31 21:51:10 +0530

    flex and grid in tailwind

--- MixedUp/Tailwind CSS - COHORT 2.0/src/App.jsx
+++ MixedUp/Tailwind CSS - COHORT 2.0/src/App.jsx
@@ -3,10 +3,22 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <div className="flex justify-between">
-        <div className="bg-red-500">Hi Their from first div</div>
-        <div className="bg-yellow-500">Hi Their from second div</div>
-        <div className="bg-blue-500">Hi Their from third div</div>
+      <div className="grid grid-cols-10">
+        <div className="bg-red-500 col-span-2">Hi This is Sidebar</div>
+        <div className="bg-yellow-500 col-span-4">
+          Hi This is main Conetent.
+        </div>
+        <div className="bg-blue-500 col-span-4">
+          Hi This is Side bar of main Content.
+        </div>
+      </div>
+
+      <div className="flex">
+        <div className="bg-red-500 w-[20%]">Hi This is Sidebar</div>
+        <div className="bg-yellow-500 w-[40%]">Hi This is main Conetent.</div>
+        <div className="bg-blue-500 w-[40%]">
+          Hi This is Side bar of main Content.
+        </div>
       </div>
     </>
   );
