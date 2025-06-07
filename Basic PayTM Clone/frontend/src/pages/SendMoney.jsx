137633f4b27322ca95d51a90e71cc044d08b1d0e
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-06-07 12:44:07 +0530

    created send money page

--- Basic PayTM Clone/frontend/src/pages/SendMoney.jsx
+++ Basic PayTM Clone/frontend/src/pages/SendMoney.jsx
@@ -2,9 +2,45 @@ import React from "react";
 
 const SendMoney = () => {
   return (
-    <div>
-      <h1>This is Send Money Page</h1>
-    </div>
+    <>
+      <div className="flex justify-center h-screen bg-slate-200">
+        <div className="h-full flex flex-col justify-center">
+          <div className="border border-slate-300 h-min text-card-foreground max-w-md p-4 space-y-8 w-96 bg-white shadow-lg rounded-lg">
+            <div className="flex flex-col space-y-1.5 p-6">
+              <h2 className="text-3xl font-bold text-center">Send Money</h2>
+            </div>
+            <div className="p-6">
+              <div className="flex items-center space-x-4">
+                <div className="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
+                  <span className="text-2xl text-white">A</span>
+                </div>
+                <h3 className="text-2xl font-semibold">Kunal Bhalerao</h3>
+              </div>
+              <div className="space-y-4">
+                <div className="space-y-2">
+                  <label
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    for="amount"
+                  >
+                    Amount (in Rs)
+                  </label>
+                  <input
+                    type="number"
+                    name="amount"
+                    id="amount"
+                    placeholder="Enter Amount"
+                    className="flex h-10 w-full rounded-md border border-slate-200 focus:border focus:border-green-300 bg-background px-3 py-2 text-sm"
+                  />
+                </div>
+                <button className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
+                  Send Money
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
