6d1235f4f4cd9f7efa0c5b7a861f4e09d56cdcc5
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-07-06 21:44:44 +0530

    Learned basic zod infer in TS

--- Week 14 - TypeScript/Lect 3 Advance APIs/src/index.ts
+++ Week 14 - TypeScript/Lect 3 Advance APIs/src/index.ts
@@ -125,3 +125,51 @@ type User = {
 // };
 
 // Exclude in TS
+
+// type checkInput = string | number | boolean
+
+// type ExcludeInput = Exclude<checkInput, boolean>
+
+// function inputAge(age: ExcludeInput) {
+//   return age
+// }
+
+// let ans = inputAge(21)
+// console.log(ans);
+
+import { z } from "zod";
+import express from "express";
+
+const app = express();
+
+// Define the schema for profile update
+const userProfileSchema = z.object({
+  name: z.string().min(1, { message: "Name cannot be empty" }),
+  email: z.string().email({ message: "Invalid email format" }),
+  age: z
+    .number()
+    .min(18, { message: "You must be at least 18 years old" })
+    .optional(),
+});
+
+type FinalUserSchema = z.infer<typeof userProfileSchema>
+
+app.get("/", (_, res) => {
+  res.send("Hello Kunal Boss")
+})
+
+app.put("/user", (req: any, res: any) => {
+  const { success } = userProfileSchema.safeParse(req.body);
+  const updateBody: FinalUserSchema = req.body; // how to assign a type to updateBody?
+
+  if (!success) {
+    res.status(411).json({});
+    return;
+  }
+  // update database here
+  res.json({
+    message: "User updated",
+  });
+});
+
+app.listen(3000);
