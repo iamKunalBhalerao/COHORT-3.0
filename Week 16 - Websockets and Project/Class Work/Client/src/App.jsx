d5889412174369879330478be86bdc543696bca3
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-07-27 01:45:28 +0530

    created frontend for websocket client

---
+++ Week 16 - Websockets and Project/Class Work/Client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+    <div id="main">
+      <input type="text" placeholder="Enter Your Message" id="message" />
+      <button id="btn">Send</button>
+    </div>
+    </>
+  );
+};
+
+export default App;
