420b7046c24daac43faab604d4df38e0c6294a5c
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-07-27 00:52:12 +0530

    Created a first websocket server

--- Week 16 - Websockets and Project/src/index.ts
+++ Week 16 - Websockets and Project/src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (socket) => {
+  console.log("User Connected");
+  setInterval(() => {
+    socket.send("Current price of solana is : " + Math.random());
+  }, 1000);
+});
