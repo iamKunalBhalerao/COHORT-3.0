4d26d59dcc7a16d9c1487a8224ccbe6111359225
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-07-27 02:06:32 +0530

    created simple ping pong application using websockets

--- Week 16 - Websockets and Project/Class Work/Client/src/App.jsx
+++ Week 16 - Websockets and Project/Class Work/Client/src/App.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [socket, setSocket] = useState();
+  const [message, setMessage] = useState("");
+
+  const sendMessage = async () => {
+    if (!socket) {
+      return;
+    }
+    socket.send(message);
+  };
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+    setSocket(ws);
+    ws.onmessage = (ev) => {
+      alert(ev.data);
+    };
+  }, []);
+
   return (
     <>
-    <div id="main">
-      <input type="text" placeholder="Enter Your Message" id="message" />
-      <button id="btn">Send</button>
-    </div>
+      <div id="main">
+        <input
+          type="text"
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Enter Your Message"
+          id="message"
+        />
+        <button onClick={sendMessage} id="btn">
+          Send
+        </button>
+      </div>
     </>
   );
 };
--- Week 16 - Websockets and Project/Class Work/Server/src/index.ts
+++ Week 16 - Websockets and Project/Class Work/Server/src/index.ts
@@ -4,14 +4,12 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", (socket) => {
   console.log("User Connected");
-  setInterval(() => {
-    socket.send("Current price of solana is : " + Math.random());
-  }, 1000);
 
   socket.on("message", (e) => {
-    console.log(e.toString());
     if (e.toString() === "ping") {
       socket.send("pong");
+    } else {
+      socket.send("Chal na Ping Bhej")
     }
   });
 });
