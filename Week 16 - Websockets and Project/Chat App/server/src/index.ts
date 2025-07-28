8850ab9382731cb2881fab5a40baab7b398606d3
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-07-28 13:00:44 +0530

    Created simple chat app server structure with room functionality

--- Week 16 - Websockets and Project/Chat App/server/src/index.ts
+++ Week 16 - Websockets and Project/Chat App/server/src/index.ts
@@ -2,14 +2,43 @@ import { WebSocket, WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let allSockets: WebSocket[] = [];
+interface User {
+  socket: WebSocket;
+  room: string;
+}
+
+let allSockets: User[] = [];
 
 wss.on("connection", (socket) => {
-  allSockets.push(socket);
   socket.on("message", (message) => {
-    for (let i = 0; i < allSockets.length; i++) {
-      const s = allSockets[i];
-      s.send(message.toString());
+    const parsedMessage = JSON.parse(message.toString());
+
+    if (parsedMessage.type === "join") {
+      allSockets.push({
+        socket,
+        room: parsedMessage.payload.rooomId,
+      });
+    }
+
+    if (parsedMessage.type === "chat") {
+      const currentUserRoom = allSockets.find((x) => x.socket == socket);
+
+      // // OR
+      // let currentUserRoom = null;
+      // for (let i = 0; i < allSockets.length; i++) {
+      //   if (allSockets[i].socket == socket) {
+      //     currentUserRoom = allSockets[i].room;
+      //   }
+      // }
+
+      for (let i = 0; i < allSockets.length; i++) {
+        if (
+          allSockets[i].room == currentUserRoom?.room &&
+          allSockets[i].socket !== socket
+        ) {
+          allSockets[i].socket.send(parsedMessage.payload.message);
+        }
+      }
     }
   });
 });
