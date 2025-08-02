caff3826e0b1d2018bd0de1a130082c6950fc32f
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-08-03 00:21:12 +0530

    Remove unwanted files

--- Brainly/Server/src/app.ts
+++ Brainly/Server/src/app.ts
@@ -8,7 +8,24 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(cors());
+// app.use(cors());
+const whitelist = ["http://localhost:5173"]; // Add Your Hosted domsain name
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true); // Allow the request
+      } else {
+        console.log(`CORS Error: Origin ${origin} not allowed.`);
+        callback(new Error("Not allowed by CORS")); // Block the request
+      }
+    },
+
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(cookieParser());
 
 // Route Imports
