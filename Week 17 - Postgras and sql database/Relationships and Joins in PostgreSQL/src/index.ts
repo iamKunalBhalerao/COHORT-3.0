9323e3dede6bb029a0eb65c875e2ef853619cde1
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-08-09 13:25:12 +0530

    Learned about types of joins

--- Week 17 - Postgras and sql database/Relationships and Joins in PostgreSQL/src/index.ts
+++ Week 17 - Postgras and sql database/Relationships and Joins in PostgreSQL/src/index.ts
@@ -120,10 +120,53 @@ app.get("/metadata", async (req, res) => {
   // const res2 = await pgClient.query(query2, [id]);
 
   // Joins in PostgreSQL
+  // const joinQuery = `
+  // SELECT users.id, users.username, users.email, users.created_at, addresses.city, addresses.country, addresses.street, addresses.pincode, addresses.created_at
+  // FROM users
+  // JOIN addresses ON users.id = addresses.user_id
+  // WHERE users.id=$1
+  // `;
+
+  // const response = await pgClient.query(joinQuery, [id]);
+
+  // Types of JOIN - 1. INNER JOIN - Find All Users With Their Addresses. If a user hasn’t filled their address, that user shouldn’t be returned
+  
+  // const joinQuery = `
+  // SELECT users.id, users.username, users.email, users.created_at, addresses.city, addresses.country, addresses.street, addresses.pincode, addresses.created_at
+  // FROM users 
+  // INNER JOIN addresses ON users.id = addresses.user_id
+  // WHERE users.id=$1 
+  // `;
+
+  // const response = await pgClient.query(joinQuery, [id]);
+
+  // // Types of JOIN - 2. LEFT join - To list all users from your database along with their address information (if they've provided it), you'd use a LEFT JOIN. Users without an address will still appear in your query result, but the address fields will be NULL for them.
+
+  // const joinQuery = `
+  // SELECT users.id, users.username, users.email, users.created_at, addresses.city, addresses.country, addresses.street, addresses.pincode, addresses.created_at
+  // FROM users 
+  // LEFT JOIN addresses ON users.id = addresses.user_id
+  // WHERE users.id=$1 
+  // `;
+
+  // const response = await pgClient.query(joinQuery, [id]);
+
+
+  // Types of JOIN - 3. RIGHT join -  Given the structure of the database, a RIGHT JOIN would be less common since the addresses table is unlikely to have entries not linked to a user due to the foreign key constraint. However, if you had a situation where you start with the addresses table and optionally include user information, this would be the theoretical use case.
+  // const joinQuery = `
+  // SELECT users.id, users.username, users.email, users.created_at, addresses.city, addresses.country, addresses.street, addresses.pincode, addresses.created_at
+  // FROM users 
+  // RIGHT JOIN addresses ON users.id = addresses.user_id
+  // WHERE users.id=$1 
+  // `;
+
+  // const response = await pgClient.query(joinQuery, [id]);
+
+  // Types of JOIN - 4. FULL Join -  A FULL JOIN would combine all records from both users and addresses, showing the relationship where it exists. Given the constraints, this might not be as relevant because every address should be linked to a user, but if there were somehow orphaned records on either side, this query would reveal them.
   const joinQuery = `
   SELECT users.id, users.username, users.email, users.created_at, addresses.city, addresses.country, addresses.street, addresses.pincode, addresses.created_at
   FROM users 
-  JOIN addresses ON users.id = addresses.user_id
+  FULL JOIN addresses ON users.id = addresses.user_id
   WHERE users.id=$1 
   `;
 
