6fe6c1850c1dfaaf916a8ad67af07645850d6d48
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-08-10 03:16:12 +0530

    Created first next js project and learned about file based routing

---
+++ Week 18 - Prisma and Start NextJS/next-start/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="w-full h-screen bg-zinc-950 flex items-center justify-center">
+        <h1>Hello, My name is Kunal</h1>
+      </div>
+    </>
+  );
+}
