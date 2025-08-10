b623df7998308a380fbd3c59f6361f49ff55beaf
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-08-10 23:33:07 +0530

    Learned about loading in next js using loading file adding in page folder

---
+++ Week 19 - Next JS Server side/server-side-in-nextjs/src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <>
+        <h1>Loading  Details ....</h1> 
+    </>
+  )
+}
+
+export default loading
--- Week 19 - Next JS Server side/server-side-in-nextjs/src/app/page.tsx
+++ Week 19 - Next JS Server side/server-side-in-nextjs/src/app/page.tsx
@@ -2,9 +2,12 @@ import axios from "axios";
 import React from "react";
 
 const page = async () => {
+  await new Promise((r) => setTimeout(r, 500));
+
   const { data } = await axios.get(
     "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
   );
+
   return (
     <div>
       {data.name}
