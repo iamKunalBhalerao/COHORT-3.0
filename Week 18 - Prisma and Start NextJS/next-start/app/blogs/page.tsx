2d41fdd4a8e835743dd753b3c993471431696525
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-08-12 21:06:34 +0530

    Some prisma stuf

---
+++ Week 18 - Prisma and Start NextJS/next-start/app/blogs/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Blogs = () => {
+  return (
+    <>
+    <div className="text-2xl font-black">
+        Learn Recoil and redux from Best Teachers
+    </div>
+    </>
+  )
+}
+
+export default Blogs
