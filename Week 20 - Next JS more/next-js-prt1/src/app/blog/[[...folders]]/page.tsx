0c6207cff605bd65aa2d499164f141fd3ddfe3bf
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-08-15 10:13:26 +0530

    add type to params argument

--- Week 20 - Next JS more/next-js-prt1/src/app/blog/[[...folders]]/page.tsx
+++ Week 20 - Next JS more/next-js-prt1/src/app/blog/[[...folders]]/page.tsx
@@ -1,3 +1,9 @@
-export default function folder({ params }) {
+export default function folder({
+  params,
+}: {
+  params: {
+    folders: string;
+  };
+}) {
   return <div>{JSON.stringify(params.folders)}</div>;
 }
