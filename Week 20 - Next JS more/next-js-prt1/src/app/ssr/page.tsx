835c3ba19c8b82218a85e4f047312c26c91da260
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-08-15 10:55:48 +0530

    some edits

--- Week 20 - Next JS more/next-js-prt1/src/app/ssr/page.tsx
+++ Week 20 - Next JS more/next-js-prt1/src/app/ssr/page.tsx
@@ -1,12 +1,10 @@
 // Server Side Rendering Code
-import Button from "@/components/Button";
 
 export default function ssr() {
     return (
         <>
             <div className="">
                 <h1>This is Server side rendering</h1>
-                <Button />
             </div>
         </>
     )
