920c1c42c8dbc68835ec2bdb913eec4653802bf7
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-08-16 00:30:15 +0530

    Impelemented google auth in next app using next auth

--- Week 20 - Next JS more/nextauth-multi-auth/src/app/api/auth/[...nextauth]/route.ts
+++ Week 20 - Next JS more/nextauth-multi-auth/src/app/api/auth/[...nextauth]/route.ts
@@ -1,49 +1,53 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
-    providers: [
-        CredentialsProvider({
-            name: "Email",
-            credentials:{ 
-                name: {
-                    label: "Full Name",
-                    type: "text",
-                    placeholder: "Enter Full Name"
-                },
-                email: {
-                    label: "Email",
-                    type: "email",
-                    placeholder: "Enter Email"
-                },
-                password: {
-                    label: "Password",
-                    type: "passwordd",
-                    placeholder: "Enter Password"
-                }
-            },
-            async authorize(credentials) {
-                const name = credentials?.name
-                const email = credentials?.email
-                const password = credentials?.password
+  providers: [
+    CredentialsProvider({
+      name: "Email",
+      credentials: {
+        name: {
+          label: "Full Name",
+          type: "text",
+          placeholder: "Enter Full Name",
+        },
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Enter Email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter Password",
+        },
+      },
+      async authorize(credentials) {
+        const name = credentials?.name;
+        const email = credentials?.email;
+        const password = credentials?.password;
 
-                const user = {
-                    id: "1",
-                    name: "Kunal",
-                    email: "kunal@gmail.com",
-                    password: "Pass@123"
-                }
+        const user = {
+          id: "1",
+          name: "Kunal",
+          email: "kunal@gmail.com",
+          password: "Pass@123",
+        };
 
-                if(user) {
-                    return user
-                } else {
-                    return null
-                }
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
 
-            }
-        })
-    ]
-    
-})
-
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
--- Week 20 - Next JS more/nextauth-multi-auth/src/app/page.tsx
+++ Week 20 - Next JS more/nextauth-multi-auth/src/app/page.tsx
@@ -1,54 +1,10 @@
-"use client";
-import { SessionProvider, signIn, signOut, useSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession();
   return (
     <>
-      <SessionProvider>
-        <RealHome />
-      </SessionProvider>
-    </>
-  );
-}
-
-export function RealHome() {
-  const session = useSession();
-
-  return (
-    <>
-      <div className="w-full h-screen flex flex-col items-center justify-self-start bg-slate-950">
-        <nav className="w-full max-w-7xl mx-auto px-4 py-2 flex items-center justify-between">
-          <h1 className="text-3xl text-blue-600">Logo</h1>
-          <div className="flex">
-            {session.status === "authenticated" && (
-              <button
-                className="text-xl px-4 py-2 bg-gray-800 rounded-lg border-1 border-slate-300 hover:bg-gray-950 transition-all duration-300 ease-in-out"
-                onClick={() => signOut()}
-              >
-                Logout
-              </button>
-            )}
-            {session.status === "unauthenticated" && (
-              <button
-                className="text-xl px-4 py-2 bg-blue-800 rounded-lg  hover:bg-blue-950 transition-all duration-300 ease-in-out"
-                onClick={() => signIn()}
-              >
-                Sign In
-              </button>
-            )}
-          </div>
-        </nav>
-        {/*  */}
-        <div className="w-full max-w-7xl pt-20">
-          {session.status === "authenticated" ? (
-            <h1 className="text-4xl text-center">
-              Welcome Back, {session.data.user?.name}
-            </h1>
-          ) : (
-            <h1 className="text-4xl text-center">Get Started</h1>
-          )}
-        </div>
-      </div>
+      <div className="">{JSON.stringify(session)}</div>
     </>
   );
 }
