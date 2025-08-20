7244a9a514515a8eb70f5f683c2bde67ad158416
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-08-20 21:58:04 +0530

    revice CSR SSR SSG

--- Week 21 - CSR, SSR, SSG/csr-ssr-ssg/src/app/page.tsx
+++ Week 21 - CSR, SSR, SSG/csr-ssr-ssg/src/app/page.tsx
@@ -1,19 +1,20 @@
 export default async function Home() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1", {
-    next: {
-      revalidate: 10
-    }
-  });
-
-  const data = response.json();
+  const data = await fetch("https://jsonplaceholder.typicode.com/todos").then(
+    (res) => res.json()
+  );
 
   return (
     <>
       <div className="">
-        {data.map((todo: any) => (
-          <div key={todo.id}>
-            <h1>{todo.title}</h1>
-            <h1>{todo.completed}</h1>
+        <h1>This is Home Page</h1>
+        {data.map((item: { id: string; title: string; completed: boolean }) => (
+          <div
+            className="bg-slate-800 rounded-lg shadow border-1 border-slate-500 p-4 my-2 flex flex-col justify-start items-start gap-2"
+            key={item.id}
+          >
+            <p>id: {item.id}</p>
+            <h1>title: {item.title}</h1>
+            <button>isCompleted: {item.completed}</button>
           </div>
         ))}
       </div>
