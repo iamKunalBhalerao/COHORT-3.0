7be806286f4e6066fdf2718f796a4ce81b4872d6
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-12-15 12:39:33 +0530

    writes logic to connect to ws server and some authentication logic

--- Week 22 - Excalidraw pt-1/draw-verse/apps/ws-server/src/server.ts
+++ Week 22 - Excalidraw pt-1/draw-verse/apps/ws-server/src/server.ts
@@ -1,21 +1,34 @@
 import { env } from "@repo/env";
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const wss = new WebSocketServer({ port: env.WS_PORT });
 
+console.log("WS-Server is Started") 
+
 wss.on("connection", function connection(ws, request) {
+  try {
+  const url = request.url;
 
-  const url = request.url
-
-  if(!url) {
-    return 
+  if (!url) {
+    return;
   }
 
-  const queryParams = new URLSearchParams(url.split("?")[1])
-  const token = queryParams.get("token")
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") || "";
+  const decoded = jwt.verify(token, env.JWT_SECRET);
+
+  if (!decoded || !(decoded as JwtPayload).email) {
+    ws.close();
+    return;
+  }
 
   ws.on("message", (message) => {
     ws.send(`Your Message: ${message}`);
   });
 
+  } catch (error: any) {
+    ws.close()  
+    console.log(error)
+  }
 });
