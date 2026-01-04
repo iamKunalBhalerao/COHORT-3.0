81ed57e45cb2fc62ad3565d7c297fe6065ff028a
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2026-01-04 11:45:25 +0530

    created simple frontend logic fot creating circles and rectangles

--- Week 23 - ExcaliDraw clone/Excalidraw-Clone/apps/excalidraw-frontend/src/app/canvas/[roomId]/page.tsx
+++ Week 23 - ExcaliDraw clone/Excalidraw-Clone/apps/excalidraw-frontend/src/app/canvas/[roomId]/page.tsx
@@ -1,18 +1,60 @@
 "use client";
 import { initDraw } from "@/draw";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-const page = () => {
+const Canvas = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [selectedTool, setSelectedTool] = useState<"rect" | "circle">("rect");
 
   useEffect(() => {
     if (canvasRef.current) {
       const canvas = canvasRef.current;
-      initDraw(canvas);
+      initDraw(canvas, selectedTool);
     }
-  }, [canvasRef]);
+  }, [selectedTool]);
 
-  return <canvas width={800} height={800} ref={canvasRef}></canvas>;
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+
+      const resizeCanvas = () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+      };
+
+      resizeCanvas();
+      window.addEventListener("resize", resizeCanvas);
+
+      return () => window.removeEventListener("resize", resizeCanvas);
+    }
+  }, []);
+
+  return (
+    <>
+      <div>
+        <canvas
+          style={{
+            display: "block",
+          }}
+          ref={canvasRef}
+        ></canvas>
+        <div className="absolute bottom-0 right-0 flex gap-2">
+          <button
+            onClick={() => setSelectedTool("rect")}
+            className={`p-2 rounded-xl cursor-pointer ${selectedTool === "rect" ? "bg-blue-500 text-white" : "bg-white text-black"} text-xl`}
+          >
+            Rect
+          </button>
+          <button
+            onClick={() => setSelectedTool("circle")}
+            className={`p-2 rounded-xl cursor-pointer ${selectedTool === "circle" ? "bg-blue-500 text-white" : "bg-white text-black"} text-xl`}
+          >
+            Clircle
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default page;
+export default Canvas;
--- Week 23 - ExcaliDraw clone/Excalidraw-Clone/apps/excalidraw-frontend/src/draw/index.ts
+++ Week 23 - ExcaliDraw clone/Excalidraw-Clone/apps/excalidraw-frontend/src/draw/index.ts
@@ -1,33 +1,112 @@
-export function initDraw(canvas: HTMLCanvasElement) {
-  const ctx = canvas.getContext("2d");
+type Shape =
+  | {
+      type: "rect";
+      x: number;
+      y: number;
+      width: number;
+      height: number;
+    }
+  | {
+      type: "circle";
+      centerX: number;
+      centerY: number;
+      radius: number;
+    };
 
+export function initDraw(
+  canvas: HTMLCanvasElement,
+  tool: "rect" | "circle" = "rect"
+) {
+  const ctx = canvas.getContext("2d");
   if (!ctx) return;
 
-  let clicked = false;
+  const existingShapes: Shape[] = [];
+
+  let isDrawing = false;
   let startX = 0;
   let startY = 0;
 
   canvas.addEventListener("mousedown", (e) => {
-    clicked = true;
+    isDrawing = true;
     startX = e.clientX;
     startY = e.clientY;
   });
 
   canvas.addEventListener("mouseup", (e) => {
-    clicked = false;
-    // console.log(e.clientX);
-    // console.log(e.clientY);
+    isDrawing = false;
+
+    if (tool === "rect") {
+      const width = e.clientX - startX;
+      const height = e.clientY - startY;
+      existingShapes.push({
+        type: "rect",
+        x: startX,
+        y: startY,
+        width,
+        height,
+      });
+    } else if (tool === "circle") {
+      const radius = Math.sqrt(
+        Math.pow(e.clientX - startX, 2) + Math.pow(e.clientY - startY, 2)
+      );
+
+      existingShapes.push({
+        type: "circle",
+        centerX: startX,
+        centerY: startY,
+        radius,
+      });
+    }
   });
 
   canvas.addEventListener("mousemove", (e) => {
-    if (clicked) {
-      const width = e.clientX - startX;
-      const height = e.clientY - startY;
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = "rgb(0, 0, 0)";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-      ctx.strokeStyle = "rgb(255, 255, 255)";
-      ctx.strokeRect(startX, startY, width, height);
+    if (isDrawing) {
+      clearCanvas(existingShapes, canvas, ctx);
+
+      if (tool === "rect") {
+        const width = e.clientX - startX;
+        const height = e.clientY - startY;
+
+        ctx.strokeStyle = "rgb(255, 255, 255)";
+        ctx.strokeRect(startX, startY, width, height);
+      } else if (tool === "circle") {
+        const radius = Math.sqrt(
+          Math.pow(e.clientX - startX, 2) + Math.pow(e.clientY - startY, 2)
+        );
+
+        ctx.strokeStyle = "rgb(255, 255, 255)";
+        ctx.beginPath();
+        ctx.arc(startX, startY, radius, 0, 2 * Math.PI);
+        ctx.stroke();
+      }
     }
   });
+}
+
+function clearCanvas(
+  existingShapes: Shape[],
+  canvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D
+) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "rgb(0, 0, 0)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  existingShapes.map((shape) => {
+    ctx.strokeStyle = "rgb(255, 255, 255)";
+
+    if (shape.type === "rect") {
+      ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+    } else if (shape.type === "circle") {
+      ctx.beginPath();
+      ctx.arc(shape.centerX, shape.centerY, shape.radius, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+  });
+}
+
+export function clearDrawing(canvas: HTMLCanvasElement) {
+  const ctx = canvas.getContext("2d");
+  if (!ctx) return;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
